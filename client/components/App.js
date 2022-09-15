698a12da032587cba2cd1dfb38280f9f058dde80
Richter Lau <ar.rick@hotmail.com>
2022-09-15 10:33:30 -0400

    setup webpack serve and nodemon

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
 function App() {
-  return <h1>react hello</h1>;
+  return (
+    <div>
+      <h1>react hello</h1>
+      <h2>testing</h2>
+      <h3>yep</h3>
+    </div>
+  );
 }
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
-const path = require('path')
+const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
   },
-  mode: 'production',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.jsx?/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader","css-loader","sass-loader"],
+      },
+    ],
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "index.html"
+      template: "index.html",
     }),
-  ]
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+      publicPath: '/build',
+    },
+  },
 };
