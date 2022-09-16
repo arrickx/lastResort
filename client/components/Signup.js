b6b112c0f3b77deb71a6e874a7f6f7e3d9fad774
Richter Lau <ar.rick@hotmail.com>
2022-09-16 14:37:38 -0400

    setup signup page

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -1,11 +1,43 @@
 import React, { useState } from "react";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function Signup() {
-  let navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`username: ${username} \npassword: ${password}`);
+  };
+
   return (
     <div>
       <h1>signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            placeholder="username"
+            autoComplete="on"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            placeholder="password"
+            autoComplete="on"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Sign up" />
+      </form>
+      <br />
       <button onClick={() => navigate("/")}>back home</button>
     </div>
   );
