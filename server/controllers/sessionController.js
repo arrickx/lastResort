1d1e913dee1bb40fb867774618d9a2e5c21d6d02
Richter Lau <ar.rick@hotmail.com>
2022-09-19 00:02:52 -0400

    update controllers

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,6 +1,17 @@
 const jwt = require("jsonwebtoken");
 const sessionController = {};
 
+sessionController.setSSID = async(req, res, next) => {
+  // console.log('setSSIDCookie .env ->',process.env.PRIVATE_KEY) // print the key
+  const ssid = jwt.sign({username: req.body.username}, process.env.PRIVATE_KEY, { expiresIn: 10 }); // 10 sec
+  
+  res.cookie('ssid', ssid, { httpOnly: true, secure: true});
+
+  res.locals.ssid = ssid;
+
+  return next();
+}
+
 sessionController.isLoggedIn = async (req, res, next) => {
   jwt.verify(req.cookies.ssid, process.env.PRIVATE_KEY, function (err, decoded) {
       if (err) {
@@ -14,5 +25,11 @@ sessionController.isLoggedIn = async (req, res, next) => {
     }
   );
 };
+
+sessionController.removeSSID = (req, res, next) => {
+  res.clearCookie('ssid')
+  res.locals.data = 'logout successfuly'
+  return next();
+}
 
 module.exports = sessionController;
