aaf7e22a4e091dc892094bdcb94f955e6f3b3cee
Richter Lau <ar.rick@hotmail.com>
2022-09-18 23:43:59 -0400

    update error page to redirect back to home page

--- client/components/Error.js
+++ client/components/Error.js
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Error() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  });
+
   return (
     <div>
       <h1>url not found</h1>
