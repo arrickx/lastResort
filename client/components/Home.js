1e3053da96b8f86d3493f1f56b90a5c39a38daf3
Richter Lau <ar.rick@hotmail.com>
2022-09-22 21:32:27 -0400

    setup basic home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -13,11 +13,12 @@ export function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1 className="text-blue-500">home</h1>
-      <img src={logo} alt="Logo" />
-      <button onClick={() => navigate('/signup')}>signup</button>
-      <button onClick={() => navigate('/login')}>login</button>
+    <div className="flex items-center justify-between m-8">
+      <img className="object-scale-down h-20 rounded-3xl" src={logo} alt="Logo" />
+      <div className="flex space-x-6">
+        <button className="items-center justify-center rounded-xl border-2 border-orange-400 bg-white-400 px-4 py-2 text-base font-medium text-orange-400 shadow-s hover:text-orange-500" onClick={() => navigate('/login')}>login</button>
+        <button className="items-center justify-center rounded-xl border border-transparent bg-orange-400 px-4 py-2 text-base font-medium text-white shadow-s hover:bg-orange-500" onClick={() => navigate('/signup')}>signup</button>
+      </div>
     </div>
   );
 }
